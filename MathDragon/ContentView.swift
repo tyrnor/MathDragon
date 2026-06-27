aa1c8389b91d8d3ce35cfb8b25dae6628fa29739
Carlos Yáñez <carlosyanez997@gmail.com>
2026-06-27 03:27:07 +0200

    Create basic layout for showing questions and answers

--- MathDragon/ContentView.swift
+++ MathDragon/ContentView.swift
@@ -9,14 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State private var currentQuestion = 0
+    
+    let questions = MultiplicationData.table2
     
     
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(questions[currentQuestion].text)
+            HStack {
+                ForEach(questions[currentQuestion].allAnswers, id: \.self) { answer in
+                    Button {
+                        if answer == questions[currentQuestion].answer {
+                            if currentQuestion < questions.count - 1 {
+                                currentQuestion += 1
+                            }
+                        }
+                    } label: {
+                        Text("\(answer)")
+                    }
+                }
+            }
         }
         .padding()
     }
