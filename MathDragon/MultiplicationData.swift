3442e9c848704d545e5b21a33c9003235b00338e
Carlos Yáñez <carlosyanez997@gmail.com>
2026-06-27 02:37:28 +0200

    Add Question struct and MultiplicationData with table2

---
+++ MathDragon/MultiplicationData.swift
@@ -0,0 +1,25 @@
+//
+//  MultiplicationData.swift
+//  MathDragon
+//
+//  Created by Carlos Yanez Puig on 27/06/2026.
+//
+
+import Foundation
+
+struct MultiplicationData {
+    static let table2 = [
+        Question(text: "2 x 1", answer: 2),
+        Question(text: "2 x 2", answer: 4),
+        Question(text: "2 x 3", answer: 6),
+        Question(text: "2 x 4", answer: 8),
+        Question(text: "2 x 5", answer: 10),
+        Question(text: "2 x 6", answer: 12),
+        Question(text: "2 x 7", answer: 14),
+        Question(text: "2 x 8", answer: 16),
+        Question(text: "2 x 9", answer: 18),
+        Question(text: "2 x 10", answer: 20),
+        Question(text: "2 x 11", answer: 22),
+        Question(text: "2 x 12", answer: 24),
+    ]
+}
---
+++ MathDragon/Question.swift
@@ -0,0 +1,13 @@
+//
+//  Question.swift
+//  MathDragon
+//
+//  Created by Carlos Yanez Puig on 27/06/2026.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: Int
+}
