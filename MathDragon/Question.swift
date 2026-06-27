4f048c1ec2946ff05ff570436dd3230ace6e9305
Carlos Yáñez <carlosyanez997@gmail.com>
2026-06-27 03:06:08 +0200

    Add allAnswers to Question

--- MathDragon/Question.swift
+++ MathDragon/Question.swift
@@ -11,4 +11,12 @@ struct Question {
     let text: String
     let answer: Int
     let wrongAnswers: [Int]
+    let allAnswers: [Int]
+    
+    init(text: String, answer: Int, wrongAnswers: [Int], allAnswers: [Int]) {
+        self.text = text
+        self.answer = answer
+        self.wrongAnswers = wrongAnswers
+        self.allAnswers = ([answer] + wrongAnswers).shuffled()
+    }
 }
