8530b84ce896eb108c985a9672a455b895f0cfbf
Carlos Yáñez <carlosyanez997@gmail.com>
2026-06-28 17:21:39 +0200

    Create Table struct

--- MathDragon/ContentView.swift
+++ MathDragon/ContentView.swift
@@ -9,29 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State private var gameState = GameState.playing
     @State private var currentQuestion = 0
     
-    let questions = MultiplicationData.table2
-    
+    let questions = MultiplicationData.table2.questions
+    let title = MultiplicationData.table2.title
     
     var body: some View {
-        VStack {
-            Text(questions[currentQuestion].text)
-            HStack {
-                ForEach(questions[currentQuestion].allAnswers, id: \.self) { answer in
-                    Button {
-                        if answer == questions[currentQuestion].answer {
-                            if currentQuestion < questions.count - 1 {
-                                currentQuestion += 1
+        NavigationStack {
+            VStack {
+                switch gameState {
+                case .playing:
+                    Text(questions[currentQuestion].text)
+                    HStack {
+                        ForEach(questions[currentQuestion].allAnswers, id: \.self) { answer in
+                            Button {
+                                if answer == questions[currentQuestion].answer {
+                                    if currentQuestion < questions.count - 1 {
+                                        currentQuestion += 1
+                                    } else {
+                                        gameState = .gameOver
+                                    }
+                                }
+                            } label: {
+                                Text("\(answer)")
                             }
                         }
-                    } label: {
-                        Text("\(answer)")
                     }
+                case .gameOver:
+                    Text("Game Over")
                 }
+                
             }
+            .padding()
+            .navigationTitle(title)
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
     }
 }
 
--- MathDragon/GameState.swift
+++ MathDragon/GameState.swift
@@ -5,3 +5,9 @@
 //  Created by Carlos Yanez Puig on 28/06/2026.
 //
 
+import Foundation
+
+enum GameState {
+    case playing
+    case gameOver
+}
